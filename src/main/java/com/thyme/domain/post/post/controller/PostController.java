53fee3389b1075ee3fa67d7ce5b37334e09b0b5d
socra167 <socra167@gmail.com>
2025-01-23 15:28:05 +0900

    docs: `validation` notation

--- src/main/java/com/thyme/domain/post/post/controller/PostController.java
+++ src/main/java/com/thyme/domain/post/post/controller/PostController.java
@@ -34,19 +34,21 @@ public class PostController {
 	// 서버로 데이터를 제출하기 위한 메서드로 POST를 사용한다
 	@PostMapping("/write")
 	@ResponseBody
-	public String doWrite(String title, String content) { // 파라미터의 이름이 같으면 그대로 사용할 수 있다
+	public String doWrite(
+			String title, // null이면 안되고, 5글자 이상이어야 한다
+			String content // null이면 안되고, 10글자 이상이어야 한다
+	) { // 파라미터의 이름이 같으면 그대로 사용할 수 있다
+		// 이렇게 데이터의 유효성을 검증하는 것을 `Validation`이라고 한다
+		// 유효성 체크는(validation) front에서도, back단에서도 한다
 		if (title.isBlank() || title == null) {
 			return getFormHtml("제목을 입력해주세요");
 		}
-
 		if (content.isBlank() || title == null) {
 			return getFormHtml("내용을 입력해주세요");
 		}
-
 		if (title.length() < 5) {
 			return getFormHtml("제목은 5글자 이상 작성해주세요");
 		}
-
 		if (content.length() < 10) {
 			return getFormHtml("내용은 10글자 이상 작성해주세요");
 		}
