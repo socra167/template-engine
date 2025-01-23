97281e338b466febb3d7bd98eb96204051c90490
socra167 <socra167@gmail.com>
2025-01-23 14:27:50 +0900

    feat: 사용자가 입력한 값을 보여주는 `doWrite()` 메서드 추가

--- src/main/java/com/thyme/domain/post/post/controller/PostController.java
+++ src/main/java/com/thyme/domain/post/post/controller/PostController.java
@@ -13,7 +13,7 @@ public class PostController {
 	@ResponseBody
 	public String showWrite() {
 		return """
-			<form action="">
+			<form action="/posts/doWrite">
 				<input type="text" name="title" placeholder="제목" />
 				<textarea name="content"></textarea>
 				<input type="submit" value="등록" />
@@ -21,6 +21,15 @@ public class PostController {
 			""";
 		// form: 사용자가 입력한 데이터를 서버로 보내준다
 		// form action: 응답을 어디로 보낼지 지정할 수 있다 '/' 하나로 앞주소가 대체된다
+	}
 
+	@GetMapping("/doWrite")
+	@ResponseBody
+	public String doWrite(String title, String content) { // 파라미터의 이름이 같으면 그대로 사용할 수 있다
+		return """
+			<h1>게시물 조회</h1>
+			<div>%s</div>
+			<div>%s</div>
+			""".formatted(title, content);
 	}
 }
